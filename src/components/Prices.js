142908e1bac765a71b796dca04ed1aad394c6274
kadambala <maheshkadambala18@gmail.com>
2022-03-29 01:14:43 +0530

    no

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -43,90 +43,90 @@ const Prices = () => {
     },
   ]);
   return (
-    <div className="prices">
-      <div className="container">
-        <Fade bottom>
-        <div className="common">
-          <h3 className="heading">{header.mainHeader}</h3>
-          <h1 className="mainHeader">{header.subHeading}</h1>
-          <p className="mainContent">{header.text}</p>
-          <div className="commonBorder"></div>
-        </div>
-        </Fade>
-        <Fade bottom>
-        <div className="row">
+<div className="prices">
+        <div className="container">
+          <Fade bottom>
+            <div className="common">
+              <h3 className="heading">{header.mainHeader}</h3>
+              <h1 className="mainHeader">{header.subHeading}</h1>
+              <p className="mainContent">{header.text}</p>
+              <div className="commonBorder"></div>
+            </div>
+          </Fade>
+          <Fade bottom>
+            <div className="row">
 
-            <div className="col-4" >
-              <div className="price">
-                <div className="priceHeading">Feedback System App</div>
-                <div className="price__rs">
-                  Learnt
-                </div>
-                <ul>
-                  <li>Feedback system app can be used to give the feedbacks.
-      Actually i have done this project for my college faculty feedback.
-      </li>
-                  <li>Java</li>
-                  <li>Xml</li>
-                  <li>Firebase</li>
-                  <li>Firebase Realtime Database</li>
-                </ul>
-                <div className="price__btn">
-                  <a href="https://github.com/iammahesh123/JntuvFeedbackSystem" className="btn btn-outline">
-                   Project Link
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-4" >
-              <div className="price">
-                <div className="priceHeading">Simple Authentication App</div>
-                <div className="price__rs">
-                  Learnt
-                </div>
-                <ul>
-                  <li>This app can be used to authenticate
-      the app via login with Email and password/OTP Authentication.</li>
-                  <li>Email Authentication</li>
-                  <li>OTP Authentication</li>
-                  <li>Firebase</li>
-                  <li>UI Designing</li>
-                </ul>
-                <div className="price__btn">
-                  <a href="https://github.com/iammahesh123/Android-Application" className="btn btn-outline">
-                   Project Link
-                  </a>
+              <div className="col-4">
+                <div className="price">
+                  <div className="priceHeading">Feedback System App</div>
+                  <div className="price__rs">
+                    Learnt
+                  </div>
+                  <ul>
+                    <li>Feedback system app can be used to give the feedbacks.
+                      Actually i have done this project for my college faculty feedback.
+                    </li>
+                    <li>Java</li>
+                    <li>Xml</li>
+                    <li>Firebase</li>
+                    <li>Firebase Realtime Database</li>
+                  </ul>
+                  <div className="price__btn">
+                    <a href="https://github.com/iammahesh123/JntuvFeedbackSystem" className="btn btn-outline">
+                      Project Link
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-4" >
-              <div className="price">
-                <div className="priceHeading">Image Objects Detection Using OpenCv</div>
-                <div className="price__rs">
-                  Learnt
-                </div>
-                <ul>
-                  <li>Image Objects Detection is a mini-project of machine learning that can be used to find out the object in GUI image.</li>
-                  <li>Python</li>
-                  <li>YOLO Algorthim</li>
-                  <li>Conventional Neural Networks
-                  </li>
-                  <li>Numpy</li>
-                  <li>OpenCv</li>
-                </ul>
-                <div className="price__btn">
-                  <a href="https://github.com/iammahesh123/Image-Object-Detection-using-openCV-with-Machine-Learning" className="btn btn-outline">
-                   Project Link
-                  </a>
+              <div className="col-4">
+                <div className="price">
+                  <div className="priceHeading">Simple Authentication App</div>
+                  <div className="price__rs">
+                    Learnt
+                  </div>
+                  <ul>
+                    <li>This app can be used to authenticate
+                      the app via login with Email and password/OTP Authentication.</li>
+                    <li>Email Authentication</li>
+                    <li>OTP Authentication</li>
+                    <li>Firebase</li>
+                    <li>UI Designing</li>
+                  </ul>
+                  <div className="price__btn">
+                    <a href="https://github.com/iammahesh123/Android-Application" className="btn btn-outline">
+                      Project Link
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-            
+              <div className="col-4">
+                <div className="price">
+                  <div className="priceHeading">Image Objects Detection Using OpenCv</div>
+                  <div className="price__rs">
+                    Learnt
+                  </div>
+                  <ul>
+                    <li>Image Objects Detection is a mini-project of machine learning that can be used to find out the object in GUI image.</li>
+                    <li>Python</li>
+                    <li>YOLO Algorthim</li>
+                    <li>Conventional Neural Networks
+                    </li>
+                    <li>Numpy</li>
+                    <li>OpenCv</li>
+                  </ul>
+                  <div className="price__btn">
+                    <a href="https://github.com/iammahesh123/Image-Object-Detection-using-openCV-with-Machine-Learning" className="btn btn-outline">
+                      Project Link
+                    </a>
+                  </div>
+                </div>
+              </div>
 
+
+            </div>
+          </Fade>
         </div>
-        </Fade>
       </div>
-    </div>
   );
 };
 
