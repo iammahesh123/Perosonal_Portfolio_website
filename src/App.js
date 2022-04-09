cbcd7c8fe5e7b9f9b38bed37e35d85a163c293ba
kadambala <maheshkadambala18@gmail.com>
2022-04-09 20:57:08 +0530

    no

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Banner from "./components/Banner";
+import Intro from "./components/Intro";
 import Nav from "./components/Nav";
-import Services from "./components/Services";
+import Skills from "./components/Skills";
 import About from "./components/About";
-import Prices from "./components/Prices";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 
 function App() {
   return (
     <div className="App">
-      <Banner />
+      <Intro />
       <Nav />
-      <Services />
-      <Prices />
+      <Skills />
+      <Projects />
       <About />
       <Contact />
     </div>
